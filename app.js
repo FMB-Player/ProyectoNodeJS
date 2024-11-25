12126c49dcd34775aa856cc44514e731a961e481
FMB_Player <franbalella@hotmail.com.ar>
2024-11-25 19:20:23 -0300

    Update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const viewRoutes = require('./frontend/routes/viewRoutes');  // Importar las rutas de vistas
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Middlewares, robado.
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Pantalla de inicio
+app.use('/', viewRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');

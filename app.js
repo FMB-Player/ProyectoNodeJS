a8e685fe0beb1c4948aa8e85752adc54e9cab3e9
FMB_Player <franbalella@hotmail.com.ar>
2024-11-25 19:48:22 -0300

    Node reset

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const viewRoutes = require('./frontend/routes/viewRoutes');  // Importar las rutas de vistas
-const cors = require('cors');
-const bodyParser = require('body-parser');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Middlewares, robado.
-app.use(cors());
-app.use(bodyParser.json());
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 
-// Pantalla de inicio
-app.use('/', viewRoutes);
+app.use(logger('dev'));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

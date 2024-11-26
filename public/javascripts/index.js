e63ca40eeb5a3c32d22f6887bdb916f43b45bdad
FMB_Player <franbalella@hotmail.com.ar>
2024-11-26 03:17:22 -0300

    25/11

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,7 @@ const KEY_PRIVATE = '31815d4440e266e989eb16be6959c875684ae745'
 const API = 'https://gateway.marvel.com:443/v1/public/';
 var ts = new Date().getTime();
 var hash = GenerateHash(ts + KEY_PRIVATE + KEY_PUBLIC).toString();
+// FIXME: El hash no es válido.
 
 document.addEventListener('DOMContentLoaded', cargarPersonajes);
 
